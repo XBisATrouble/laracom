dd1bc0ce05d936c697c0337aa2899c08d733e399
xbisatrouble <xbisatrouble@gmail.com>
2022-04-01 02:24:15 +0800

    tiny

--- project/app/Http/Controllers/Front/ContactController.php
+++ project/app/Http/Controllers/Front/ContactController.php
@@ -26,7 +26,14 @@ class ContactController
 
     public function store()
     {
-        Mail::to("xbisatrouble@gmail.com")->send("test");
+        $name = '王宝花';
+        // Mail::send()的返回值为空，所以可以其他方法进行判断
+        Mail::send('emails.test',['name'=>$name],function($message){
+            $to = 'xbisatrouble@gmail.com';
+            $message ->to($to)->subject('邮件测试');
+        });
+        // 返回的一个错误数组，利用此可以判断是否发送成功
+        dd(Mail::failures());
         return view('front.contact',["data"=>"Success"]);
     }
 }
