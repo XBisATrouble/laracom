76ebe92dcd0cca25efc057339d82b00c7b30ff8c
xbisatrouble <xbisatrouble@gmail.com>
2022-04-01 20:35:28 +0800

    tiny

--- project/app/Shop/Customers/Customer.php
+++ project/app/Shop/Customers/Customer.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Nicolaslopezj\Searchable\SearchableTrait;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class Customer extends Authenticatable
+class Customer extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable, SoftDeletes, SearchableTrait;
 
